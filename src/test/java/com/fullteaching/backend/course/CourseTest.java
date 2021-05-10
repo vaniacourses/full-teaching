86e577016d368b1c3062387b85a5df9941a0c600
Leandro Shibahara <leandrohideyuki@hotmail.com>
2021-05-10 02:21:07 -0300

    Criand testes unitários para course e coursedetails

---
+++ src/test/java/com/fullteaching/backend/course/CourseTest.java
@@ -0,0 +1,107 @@
+package com.fullteaching.backend.course;
+import com.fullteaching.backend.coursedetails.CourseDetails;
+import com.fullteaching.backend.session.Session;
+import com.fullteaching.backend.user.User;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.mock;
+
+public class CourseTest {
+
+
+    @Test
+    public void createCourse() {
+        Course course = new Course();
+        Assertions.assertEquals(0, course.getId());
+    }
+
+
+    @Test
+    public void createCourseWithTeacher() {
+        User teacher = mock(User.class);
+        String title = "title test";
+        String image = "image test";
+        Course course = new Course(title, image, teacher);
+        Assertions.assertEquals("title test", course.getTitle());
+        Assertions.assertEquals("image test", course.getImage());
+    }
+
+
+    @Test
+    public void createCourseWithTeacherAndCourseDetails() {
+        User teacher = mock(User.class);
+        CourseDetails courseDetails = mock(CourseDetails.class);
+        Mockito.when(courseDetails.getInfo()).thenReturn("info test");
+        String title = "title test";
+        String image = "image test";
+        Course course = new Course(title, image, teacher, courseDetails);
+        Assertions.assertEquals("title test", course.getTitle());
+        Assertions.assertEquals("image test", course.getImage());
+        Assertions.assertEquals("info test", courseDetails.getInfo());
+    }
+
+    @Test
+    public void testId() {
+        Course course = new Course();
+        course.setId(3);
+        Assertions.assertEquals(3, course.getId());
+    }
+
+    @Test
+    public void testTitle() {
+        Course course = new Course();
+        course.setTitle("title test");
+        Assertions.assertEquals("title test", course.getTitle());
+    }
+
+    @Test
+    public void testImage() {
+        Course course = new Course();
+        course.setImage("image test");
+        Assertions.assertEquals("image test", course.getImage());
+    }
+
+    @Test
+    public void testTeacher() {
+        User teacher = mock(User.class);
+        Mockito.when(teacher.getName()).thenReturn("Teacher");
+        Assertions.assertEquals("Teacher", teacher.getName());
+    }
+
+
+    @Test
+    public void testCourseDetails() {
+        CourseDetails courseDetails = mock(CourseDetails.class);
+        Mockito.when(courseDetails.getInfo()).thenReturn("info test");
+        Course course = new Course();
+        course.setCourseDetails(courseDetails);
+        Assertions.assertEquals("info test", course.getCourseDetails().getInfo());
+    }
+
+    @Test
+    public void testAttenders() {
+        User teacher = mock(User.class);
+        Mockito.when(teacher.getName()).thenReturn("Teacher");
+        Course course = new Course();
+        Set<User> user = new HashSet<User>();
+        user.add(teacher);
+        course.setAttenders(user);
+        Assertions.assertEquals(user, course.getAttenders());
+    }
+
+    @Test
+    public void testSessions() {
+        Session session = mock(Session.class);
+        Mockito.when(session.getTitle()).thenReturn("Session");
+        Course course = new Course();
+        Set<Session> sessionList = new HashSet<Session>();
+        sessionList.add(session);
+        course.setSessions(sessionList);
+        Assertions.assertEquals(sessionList, course.getSessions());
+    }
+} 
---
+++ src/test/java/com/fullteaching/backend/coursedetails/CourseDetailsTest.java
@@ -0,0 +1,73 @@
+package com.fullteaching.backend.coursedetails;
+import com.fullteaching.backend.course.Course;
+import com.fullteaching.backend.filegroup.FileGroup;
+import com.fullteaching.backend.forum.Forum;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+public class courseDetailsTest {
+
+    @Test
+    public void createCourseDetails() {
+        CourseDetails courseDetails = new CourseDetails();
+        Assertions.assertEquals(0, courseDetails.getId());
+    }
+
+    @Test
+    public void createCourseDetailsWithCourse() {
+        Course course = mock(Course.class);
+        Mockito.when(course.getTitle()).thenReturn("title test");
+        CourseDetails courseDetails = new CourseDetails(course);
+        Assertions.assertEquals("title test", courseDetails.getCourse().getTitle());
+    }
+
+
+    @Test
+    public void testId() {
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setId(3);
+        Assertions.assertEquals(3, courseDetails.getId());
+    }
+
+    @Test
+    public void testInfo() {
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setInfo("info test");
+        Assertions.assertEquals("info test", courseDetails.getInfo());
+    }
+
+    @Test
+    public void testForum() {
+        Forum forum = mock(Forum.class);
+        Mockito.when(forum.isActivated()).thenReturn(true);
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setForum(forum);
+        Assertions.assertEquals(true, courseDetails.getForum().isActivated());
+    }
+
+    @Test
+    public void testFiles() {
+        CourseDetails courseDetails = new CourseDetails();
+        FileGroup fileGroup = mock(FileGroup.class);
+        Mockito.when(fileGroup.getTitle()).thenReturn("file group test");
+        List<FileGroup> listFileGroup = new ArrayList<FileGroup>();
+        listFileGroup.add(fileGroup);
+        courseDetails.setFiles(listFileGroup);
+        Assertions.assertEquals(listFileGroup, courseDetails.getFiles());
+    }
+
+    @Test
+    public void testCourse() {
+        Course course = mock(Course.class);
+        Mockito.when(course.getTitle()).thenReturn("course test");
+        CourseDetails courseDetails = new CourseDetails();
+        courseDetails.setCourse(course);
+        Assertions.assertEquals("course test", courseDetails.getCourse().getTitle());
+    }
+} 
